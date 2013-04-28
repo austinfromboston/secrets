f2808efa2d76df16fe822520b932332ca7314cae
Austin Putman, austinfromboston <austin@rawfingertips.com>
2013-04-27 17:10:06 -0700

    help secrets be more forgiving in test environment

--- lib/secrets.rb
+++ lib/secrets.rb
@@ -2,6 +2,8 @@ require "rails"
 require "ostruct"
 
 class Secrets < Rails::Railtie
+  class NotEnoughSecrets < StandardError; end
+
   config.secrets_path = "config/secrets.yml"
   
   initializer "secrets.load" do |_|
@@ -12,11 +14,16 @@ class Secrets < Rails::Railtie
     Object.send(:remove_const, :Secret) if defined? Secret
     secrets = {}
     
-    Array(config.secrets_path).each do |path|
+    Array("config/secrets.yml").each do |path|
       next unless File.exist?(Rails.root + path)
 
-      YAML.load_file(Rails.root.join config.secrets_path)[Rails.env.to_s].each do |(key, value)|
-        secrets[key] = value.is_a?(Hash) ? OpenStruct.new(value) : value
+      secrets_file_values = YAML.load_file(Rails.root.join path)[Rails.env.to_s]
+      if secrets_file_values
+        secrets_file_values.each do |(key, value)|
+          secrets[key] = value.is_a?(Hash) ? OpenStruct.new(value) : value
+        end
+      else
+        raise NotEnoughSecrets unless Rails.env.test?
       end
 
     end
